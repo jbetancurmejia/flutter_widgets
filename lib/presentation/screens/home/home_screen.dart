c708a3e9189db7a8e4f65f6c9fe856e1d9a3ccf7
camilo betancur <jcbmdeveloper@gmail.com>
2023-09-28 20:06:48 -0500

    go router configured

--- lib/presentation/screens/home/home_screen.dart
+++ lib/presentation/screens/home/home_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
 import 'package:widgets_app/config/menu/menu_items.dart';
 import 'package:widgets_app/presentation/screens/buttons/buttons_screen.dart';
 
@@ -48,14 +49,14 @@ class _CustomMenuItem extends StatelessWidget {
       subtitle: Text(menuItem.subTitle),
       trailing: Icon(Icons.arrow_forward_ios_rounded, color: colors.secondary),
       onTap: () {
-
         // Navigator.of(context).push(
         //   MaterialPageRoute(
         //     builder: (context) =>  const ButtonsScreen(),
         //   ),
         // );
 
-        Navigator.pushNamed(context, menuItem.link);
+        // Navigator.pushNamed(context, menuItem.link);
+        context.push(menuItem.link);
       },
     );
   }
